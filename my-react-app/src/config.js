3d07718eef476b58831382656f99a9f86de26b0f
andrew <afoudriat1@gmail.com>
2025-11-18 19:10:14 -0800

    Set frontend to VM mode and add ngrok bypass headers

--- my-react-app/src/config.js
+++ my-react-app/src/config.js
@@ -2,7 +2,7 @@
 // Change this to switch between local and VM deployment
 
 // Set to 'local' for local development, 'vm' for VM deployment
-const DEPLOYMENT = 'local'; // Change to 'vm' when deploying to VM
+const DEPLOYMENT = 'vm'; // Change to 'vm' when deploying to VM
 
 const API_BASE_URL = DEPLOYMENT === 'vm' 
   ? 'http://34.56.119.174:5001'
